ca8a50966f720135d9807d26d45736449bb149a2
Martin Schnabel <mb0@mb0.org>
2022-11-21 20:21:22 +0100

    qry: use lit vals instead of list in literal backends
    It would nice to use any idxr implementation directly to reduce allocations
    and allow lazy indexer implementations for larger streams, but it would
    also complicate the code considerably.

--- qry/dom.go
+++ qry/dom.go
@@ -14,34 +14,26 @@ type DomBackend struct {
 
 func (b *DomBackend) Proj() *dom.Project { return b.Project }
 func (b *DomBackend) Exec(p *exp.Prog, j *Job) (*exp.Lit, error) {
-	var list *lit.List
+	var vals lit.Vals
 	switch j.Ref {
 	case "dom.model":
-		list = &lit.List{Vals: make([]lit.Val, 0, len(b.Schemas)*8)}
+		vals = make([]lit.Val, 0, len(b.Schemas)*8)
 		for _, s := range b.Schemas {
 			for _, m := range s.Models {
 				x, _ := p.Reg.Proxy(m)
-				if len(list.Vals) == 0 {
-					list.El = x.Type()
-				}
-				list.Vals = append(list.Vals, x)
+				vals = append(vals, x)
 			}
 		}
 	case "dom.schema":
-		list = &lit.List{Vals: make([]lit.Val, 0, len(b.Schemas))}
+		vals = make([]lit.Val, 0, len(b.Schemas))
 		for _, s := range b.Schemas {
 			x, _ := p.Reg.Proxy(s)
-			if len(list.Vals) == 0 {
-				list.El = x.Type()
-			}
-			list.Vals = append(list.Vals, x)
+			vals = append(vals, x)
 		}
 	case "dom.project":
-		x, _ := p.Reg.Proxy(b.Project)
-		list = &lit.List{El: x.Type(), Vals: []lit.Val{x}}
+		vals = lit.Vals{lit.MustProxy(p.Reg, b.Project)}
+	default:
+		return nil, fmt.Errorf("dom backend: unexpected ref %s", j.Ref)
 	}
-	if list == nil {
-		return nil, fmt.Errorf("dom backend: not implemented")
-	}
-	return execListQry(p, j, list)
+	return execListQry(p, j, vals)
 }
--- qry/lit.go
+++ qry/lit.go
@@ -21,25 +21,25 @@ func (b *LitBackend) Exec(p *exp.Prog, j *Job) (*exp.Lit, error) {
 	if err != nil {
 		return nil, fmt.Errorf("lit backend: %w", err)
 	}
-	var list *lit.List
-	switch v := a.Val.(type) {
+	var vals lit.Vals
+	switch v := a.Value().(type) {
+	case *lit.Vals:
+		vals = *v
 	case *lit.List:
-		list = v
+		vals = v.Vals
 	case lit.Idxr:
-		vs := make([]lit.Val, 0, v.Len())
+		vals = make(lit.Vals, 0, v.Len())
 		v.IterIdx(func(idx int, el lit.Val) error {
-			vs = append(vs, el)
+			vals = append(vals, el)
 			return nil
 		})
-		list = &lit.List{Vals: vs}
-	}
-	if list == nil {
+	default:
 		return nil, fmt.Errorf("literal query expects list got %T", a)
 	}
-	return execListQry(p, j, list)
+	return execListQry(p, j, vals)
 }
 
-// MemBackend is a query backend that evaluates queries using xelf on in-memory literal values.
+// MemBackend is a query backend that evaluates queries using in-memory literal values.
 type MemBackend struct {
 	*lit.Reg
 	*dom.Project
@@ -69,7 +69,7 @@ func (b *MemBackend) Stream(key string) (mig.Stream, error) {
 	return mig.NewLitStream(b.list(m)), nil
 }
 func (b *MemBackend) Exec(p *exp.Prog, j *Job) (*exp.Lit, error) {
-	return execListQry(p, j, b.list(j.Model))
+	return execListQry(p, j, b.list(j.Model).Vals)
 }
 func (b *MemBackend) list(m *dom.Model) (list *lit.List) {
 	if list = b.Data[m.Qualified()]; list == nil {
@@ -94,43 +94,42 @@ func (b *MemBackend) Add(m *dom.Model, list *lit.Vals) error {
 }
 
 var _ mig.Dataset = (*MemBackend)(nil)
-var andSpec = lib.And
 
-func execListQry(p *exp.Prog, j *Job, list *lit.List) (*exp.Lit, error) {
+func execListQry(p *exp.Prog, j *Job, vals lit.Vals) (*exp.Lit, error) {
 	var whr exp.Exp
 	if len(j.Whr) > 0 {
-		whr = &exp.Call{Args: append([]exp.Exp{exp.LitVal(andSpec)}, j.Whr...)}
+		whr = &exp.Call{Args: append([]exp.Exp{exp.LitVal(lib.And)}, j.Whr...)}
 	}
 	if j.Kind == KindCount {
-		return collectCount(p, j, list, whr)
+		return collectCount(p, j, vals, whr)
 	}
-	res, err := collectList(p, j, list, whr)
+	res, err := collectList(p, j, vals, whr)
 	if err != nil {
 		return nil, err
 	}
 	l := &exp.Lit{Res: j.Res}
 	switch j.Kind {
 	case KindOne:
-		if len(res.Vals) == 0 {
+		if len(res) == 0 {
 			l.Val = lit.Null{}
 		} else {
-			l.Val = res.Vals[0]
+			l.Val = res[0]
 		}
 	case KindMany:
-		l.Val = res
+		l.Val = &lit.List{El: typ.El(j.Res), Vals: res}
 	default:
 		return nil, fmt.Errorf("exec unknown query kind %s", j.Ref)
 	}
 	return l, nil
 }
 
-func collectList(p *exp.Prog, j *Job, list *lit.List, whr exp.Exp) (*lit.List, error) {
-	res := make([]lit.Val, 0, len(list.Vals))
-	org := list.Vals
+func collectList(p *exp.Prog, j *Job, vals lit.Vals, whr exp.Exp) (res lit.Vals, _ error) {
+	res = make([]lit.Val, 0, len(vals))
+	org := vals
 	if whr != nil {
-		org = make([]lit.Val, 0, len(list.Vals))
+		org = make([]lit.Val, 0, len(vals))
 	}
-	for _, l := range list.Vals {
+	for _, l := range vals {
 		j.Cur = l
 		if whr != nil {
 			ok, err := filter(p, j, l, whr)
@@ -194,16 +193,16 @@ func collectList(p *exp.Prog, j *Job, list *lit.List, whr exp.Exp) (*lit.List, e
 	if j.Lim > 0 && len(res) > int(j.Lim) {
 		res = res[:j.Lim]
 	}
-	return &lit.List{El: typ.El(j.Res), Vals: res}, nil
+	return res, nil
 }
 
-func collectCount(p *exp.Prog, j *Job, list *lit.List, whr exp.Exp) (*exp.Lit, error) {
+func collectCount(p *exp.Prog, j *Job, vals lit.Vals, whr exp.Exp) (*exp.Lit, error) {
 	// we can ignore order and selection completely
 	var res int64
 	if whr == nil {
-		res = int64(len(list.Vals))
+		res = int64(len(vals))
 	} else {
-		for _, l := range list.Vals {
+		for _, l := range vals {
 			j.Cur = l
 			ok, err := filter(p, j, l, whr)
 			if err != nil {
@@ -228,7 +227,7 @@ func collectCount(p *exp.Prog, j *Job, list *lit.List, whr exp.Exp) (*exp.Lit, e
 	return exp.LitVal(lit.Int(res)), nil
 }
 
-func filter(p *exp.Prog, env exp.Env, l lit.Val, whr exp.Exp) (bool, error) {
+func filter(p *exp.Prog, env exp.Env, v lit.Val, whr exp.Exp) (bool, error) {
 	whr, err := p.Resl(env, whr, typ.Bool)
 	if err != nil {
 		return false, err
@@ -244,7 +243,7 @@ func filter(p *exp.Prog, env exp.Env, l lit.Val, whr exp.Exp) (bool, error) {
 	return b == true, nil
 }
 
-func orderResult(sel, subj []lit.Val, ords []Ord) (res error) {
+func orderResult(sel, subj lit.Vals, ords []Ord) (res error) {
 	sort.Stable(orderer{sel, subj, func(i, j int) bool {
 		less, err := orderFunc(sel, subj, i, j, ords)
 		if err != nil && res == nil {
@@ -255,7 +254,7 @@ func orderResult(sel, subj []lit.Val, ords []Ord) (res error) {
 	return res
 }
 
-func orderFunc(sel, subj []lit.Val, i, j int, ords []Ord) (bool, error) {
+func orderFunc(sel, subj lit.Vals, i, j int, ords []Ord) (bool, error) {
 	ord := ords[0]
 	list := sel
 	if ord.Subj {
@@ -283,7 +282,7 @@ func orderFunc(sel, subj []lit.Val, i, j int, ords []Ord) (bool, error) {
 }
 
 type orderer struct {
-	a, b []lit.Val
+	a, b lit.Vals
 	less func(i, j int) bool
 }
 
